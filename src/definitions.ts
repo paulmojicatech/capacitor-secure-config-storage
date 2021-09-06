e168530b5a483b18cb619e1a4440b965bbc2d11d
Paul Mojica <paulmojicatech@gmail.com>
2021-09-05 21:19:12 -0400

    create interface for plugin

--- src/definitions.ts
+++ src/definitions.ts
@@ -1,3 +1,7 @@
 export interface SecureConfigStoragePlugin {
   echo(options: { value: string }): Promise<{ value: string }>;
+
+  getConfigValue(key: string): Promise<{ value: string}>;
+  storeConfigValue(key: string): Promise<void>;
+
 }

3b3e56c1c85018bae0dbc4fe7e70999e3195d621
Paul Mojica <paulmojicatech@gmail.com>
2021-09-06 21:41:29 -0400

    add logic for getting client secret

--- ios/Plugin/SecureConfigStorage.swift
+++ ios/Plugin/SecureConfigStorage.swift
@@ -6,6 +6,8 @@ import Foundation
     }
     
     @objc public func getValueFromConfig(_ value: String) -> String {
-        return "Test"
+        let path = Bundle.main.path(forResource: "Info", ofType: "plist") as! String
+        let infoContents = NSDictionary(contentsOfFile: path) as! [String:AnyObject]
+        return infoContents["ClientSecret"] as! String
     }
 }
